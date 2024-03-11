89a553d3c292e3ce1780ecdd0c94124328945fdc
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-03-11 21:11:53 +0100

    Task1

---
+++ Module_4/TAS_JAVA/src/Tasks/Task_1.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Bukola");
+        System.out.println("I am a QA engineer");
+        System.out.println("I am a Yoruba");
+        System.out.println("I am a Java");
+        System.out.println("I love God");
+
+
+    }
+}
