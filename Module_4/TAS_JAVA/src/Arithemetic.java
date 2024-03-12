fa4eee718df12a5641cfb13e61b074cc70c61371
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-03-12 11:38:51 +0100

    Task 4

---
+++ Module_4/TAS_JAVA/src/Arithemetic.java
@@ -0,0 +1,24 @@
+public class Arithemetic {
+    public static void main(String[] args) {
+        //basic arithmetic
+        byte age1 =20;
+        byte age2 =30;
+         /*System.out.println(age1+age2);
+        System.out.println(age1-age2);
+        System.out.println(age1/age2);*/
+
+        //increment and decrement
+        //age1++;
+       // age2--;
+       // System.out.println(age2);
+
+        //compound assignment operator -= += /= *=
+        age1-=age2;
+        //20-30=-10
+        //age1=-10
+        System.out.println(age1);
+
+
+
+    }
+}
---
+++ Module_4/TAS_JAVA/src/Tasks/Task_4.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        // Given radius
+        double radius = 12.7;
+
+        // Calculate area using the formula
+        double area = 3.14 * radius * radius;
+
+        // Print the result with the unit of measurement
+        System.out.println("The area of the circle with radius " + radius + " meters is: " + area + " square meters");
+    }
+}
