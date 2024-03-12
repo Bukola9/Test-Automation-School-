af71980e9d7a173c2174f910d40fd765e27d2db0
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-03-12 01:08:52 +0100

    Task2

---
+++ Module_4/TAS_JAVA/src/JavaDataType.java
@@ -0,0 +1,15 @@
+public class JavaDataType {
+    public static void main(String[] args) {
+        byte age =-127;
+        short money = 32_76;
+        long SoMuchMoney =-24234565;
+        float area = 1234567.9f;
+        double BiggerAreas =123456.67;
+        char option ='A';
+        Boolean isAround = false;
+
+        System.out.println(area);
+
+    }
+}
+
---
+++ Module_4/TAS_JAVA/src/Tasks/Task_2.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        byte age =-127;
+        short money = 32_76;
+        long SoMuchMoney =-24234565;
+        float area = 1234567.9f;
+        double BiggerAreas =123456.67;
+        char option ='A';
+        Boolean isAround = false;
+
+        System.out.println(area);
+        System.out.println(age);
+        System.out.println(money);
+        System.out.println(SoMuchMoney);
+        System.out.println(BiggerAreas);
+        System.out.println(option);
+        System.out.println(isAround);
+    }
+}
