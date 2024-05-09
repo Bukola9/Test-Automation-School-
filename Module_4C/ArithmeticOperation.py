c87eb8180c04d889c6ae7d5f45eff19be7c82bdb
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-05-09 15:32:39 +0100

    third task

---
+++ Module_4C/ArithmeticOperation.py
@@ -0,0 +1,38 @@
+result = 6+4
+print(result)
+
+result = 6-4
+print(result)
+
+result = 6*4
+print(result)
+
+result = 6/2
+print(result)
+
+result = 6%2
+print(result)
+
+#Compound Arithmetic Operation
+result = 6
+result+=4
+print(result)
+
+result = 6
+result-=4
+print(result)
+
+result = 6
+result*=4
+print(result)
+
+print("increment and decrement")
+result = 6
+result+=1
+
+print(result)
+
+result = 6
+result-=1
+print(result)
+
---
+++ Module_4C/task4.py
@@ -0,0 +1,4 @@
+First_num = 45
+Second_num =34
+Answer = First_num * Second_num
+print(Answer)
