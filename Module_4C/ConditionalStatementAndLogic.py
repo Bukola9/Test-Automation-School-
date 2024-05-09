5b4ec928426d51eeaedada716365520487186c0d
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-05-09 17:28:13 +0100

    sixth task

---
+++ Module_4C/ConditionalStatementAndLogic.py
@@ -0,0 +1,20 @@
+print("Logical and\n")
+number1 = 10
+number2 = 20
+if number1==10 and number2==20:
+    print("Number1==10, number2==20")
+
+if number1==5 and number2==20:
+    print("Number1 =5, Number2=20")
+
+print("logical or\n")
+
+if number1==10 or number2==20:
+    print("Number1 =10, Number2 =20")
+
+if number1 == 5 or number2 == 20:
+        print("Number1 =5, Number2 =20")
+
+print("logical not\n")
+if not number1==10:
+    print("NUmber is 10")
---
+++ Module_4C/task6.py
@@ -0,0 +1,15 @@
+number1=60
+number2=30
+if number1>50:
+    print("Evaluation is true")
+else:
+    print("Evaluation is false")
+
+if number2<100:
+    print("Evaluation is true")
+else:
+    print("Evaluation is false")
+
+name = "python"
+if name == "python" or "testify":
+    print("Yay!! The string is a Testify Python")
