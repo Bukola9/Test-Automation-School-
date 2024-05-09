cc2e8c47f53f6d55eeb21f991931699c32d1a31d
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-05-09 13:59:06 +0100

    third task

---
+++ Module_4C/Lesson3.py
@@ -0,0 +1,13 @@
+name = "testify"
+lesson = "Python, string and concatenation"
+
+#escape sequence
+article = "this is article\na multiline article\n\tEach text.\b"
+
+article2 = r"this is article\na multiline article\n\tEach text.\b"
+print(article2)
+
+group = "wood"
+attr = "peckers"
+bird = group+attr
+print(bird)
---
+++ Module_4C/task3.py
@@ -0,0 +1,5 @@
+raw_string = r"this is article\na multiline article\n\tEach text.\b"
+first_name = "Oluwa"
+last_name = "bukola"
+full_name = first_name+last_name
+print(full_name)
