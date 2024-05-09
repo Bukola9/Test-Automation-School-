a2f17125c7a8e1492ded7ae515ec27c2d528bf6a
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-05-09 21:31:40 +0100

    Loop and task7

---
+++ Module_4C/Loop.py
@@ -0,0 +1,25 @@
+#For Loop
+print("For loop\n")
+
+#iterate sequence
+fruits = ["Apple", "Banana", "Coconut"]
+for fruit in fruits:
+    print("fruit:  ", fruit)
+
+name = "python"
+for ch in name:
+    print("Character:", ch)
+
+
+number = 5
+for i in range(number):
+    print("number  ", i)
+
+#While loop
+print("\nwhile loop\n")
+
+number=10
+
+while number >0:
+    print("Number is", number)
+    number-=1
---
+++ Module_4C/task7.py
@@ -0,0 +1,3 @@
+number = 11
+for i in range(number):
+    print("Hello World" , i)
