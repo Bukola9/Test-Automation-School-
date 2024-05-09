6f7f7eb9dc59389c7df9539fd07ce48c4593290b
Bukola Adeniran <adeniranbukolasamuel@gmail.com>
2024-05-09 13:21:09 +0100

    first task

---
+++ Module_4C/Lesson2.py
@@ -0,0 +1,49 @@
+
+#integer
+number1 = 1029929292
+number2 =-192039939
+long_number=2000300102020202020202002020202022020020020202020202
+
+print(type(number1))    #int
+print(type(number2))    #int
+print(type(long_number))    #int
+
+#float
+float1 = 12.34
+float2 =12.  #12.0
+float3 = .34  #0.34
+float4 =12e7
+
+print(type(float1))  #float
+print(type(float2)) #float
+print(type(float3)) #float
+print(type(float4)) #float
+
+#complex number
+complex_num=2+1j
+print(type(complex_num))  #complex
+
+
+
+#String
+testify = "testify"
+language ='python'
+
+print(type(testify))  #String
+print(type(language)) #String
+
+
+#boolean
+is_java = False
+is_python = True
+print(type(is_java))
+print(type(is_python))
+
+
+name =10 #int
+print(type(name))
+name ="Bukola"  #String
+print(type(name))
+name = False #Boolean
+print(type(name))
+
